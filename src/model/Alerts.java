bbc4a884286ac4b175f1a2e6a1175843e52d2b7b
RaMcDaniel <rmcda35@wgu.edu>
2022-02-06 23:01:54 -0500

    added generic error class

---
+++ src/model/Alerts.java
@@ -0,0 +1,26 @@
+package model;
+
+import javafx.scene.control.Alert;
+
+/** This class contains alerts that are referenced elsewhere in the program.
+ * Alerts include........
+ * and error messages in case of inappropriate user input.
+ */
+public class Alerts {
+
+    public static Alert inventory = new Alert(Alert.AlertType.ERROR, "Inventory must be greater than or equal to minimum. Inventory must be less than or equal to maximum.");
+    public static Alert cancel = new Alert(Alert.AlertType.CONFIRMATION, "'Cancel' will lose all work on this page and return to main. Would you like to continue?");
+
+    /** This is a general purpose alert creator. A popup box appears telling the user how to correct their input.
+     * It can be generalized to any field in the program.
+     * @param inputField The field with the inappropriate input.
+     * @param howToCorrect A string describing acceptable inputs.
+     * @return An alert describing to user how to fix input.
+     */
+    public static Alert inputError (String inputField, String howToCorrect){
+        String fullWarning = String.format("Please correct %s. This field only accepts: %s", inputField, howToCorrect);
+        Alert inputError = new Alert(Alert.AlertType.ERROR, fullWarning);
+        return inputError;
+    }
+
+}
